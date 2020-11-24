10c2ae413d547a027bf4ed59497ef7f240efcd1c
Lazar Jelic <lazarjelic98@gmail.com>
2020-11-24 19:19:46 +0100

    Added grader, Added tests

---
+++ acinonyx/test/grader.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+wd=$(pwd)
+cd $(dirname ${BASH_SOURCE[0]})
+for i in `find . ! -path . -type d | sort`; do
+	rm -f "$i/src" "$i/src.o" "$i/gen" && ok=true
+    continue
+	fpc "$i/src.pas" &> /dev/null
+	make "$i/gen" &> /dev/null
+    for j in {1..5}; do
+        t="$i/$j" src=$("$i/src" <"$t.in") gen=$("$i/gen" <"$t.in") out=$(<"$t.out")
+		if [ "$src" != "$out" ] || [ "$gen" != "$out" ]; then
+			printf "$t\tERROR\n\nIN\n$(<"$t.in")\n\nSRC\n$src\n\nGEN\n$gen\n\nOUT\n$out\n"
+			ok=false && break
+		fi
+	done
+	$ok && printf "$i\tOK\n"
+done
+cd $wd
